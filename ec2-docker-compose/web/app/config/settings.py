8cf847493939525194c1d09824e19410bb53b65b
himanmenGit <study.himanmen@gmail.com>
2019-04-04 11:51:24 +0900

    ec2-docker-compose 추가

--- ec2-docker-compose/web/app/config/settings.py
+++ ec2-docker-compose/web/app/config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'ijd$5vq=^(729tbmv-!hbye%p2y4^mu6l7@5t+=%7f@re==xtg'
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    '.amazonaws.com'
+    '*'
 ]
 
 
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'password123',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
